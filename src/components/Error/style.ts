5d4d31fd4599fd7326c0d984c9855bc997222e11
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-17 20:26:30 -0300

    chore: Add position absolute and centered the element

--- src/components/Error/style.ts
+++ src/components/Error/style.ts
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 export const Content = styled.div`
