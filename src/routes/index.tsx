cb94620fbe319a387ca69e3b930b3270d99adc1a
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-17 23:07:35 -0300

    feat: create PrivateRoute structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {
+  BrowserRouter, 
+  Switch, 
+  Route, 
+  Redirect, 
+  RouteProps as ReactDOMRouteProps,
+} from 'react-router-dom';
 
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import ForgotPassword from '../pages/ForgotPassword';
 import Dashboard from '../pages/Dashboard';
 
+import { isAuth } from '../services/isAuth';
+
+interface RouteProps extends ReactDOMRouteProps { 
+  component: React.ComponentType;
+} 
+
+const PrivateRoute: React.FC<RouteProps> = ({ component: Component, ...rest }) => (
+  <Route 
+    {...rest} 
+    render={
+      props => isAuth 
+      ? (<Component />)
+      : (<Redirect to={{pathname: '/', state: { from: props.location}}} />)
+    }
+  />
+);
 const Routes: React.FC = () => {
+
+
   return(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={SignIn} />
         <Route path="/forgot-password" exact component={ForgotPassword} />
-        <Route path="/dashboard" exact component={Dashboard} />
+        {/* <Route path="/dashboard" exact component={Dashboard} /> */}
+        <PrivateRoute path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/services/isAuth.ts
@@ -0,0 +1,4 @@
+export const TOKEN_KEY: string = '@tokenApp';
+export const isAuth = null || localStorage.getItem('@tokenApp');
+
+
