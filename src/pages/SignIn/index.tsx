5cd0570c813ba37b9df300734a9d05069039dd34
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-17 21:49:39 -0300

    feat: add form login functions for call api

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { FormEvent, useState} from 'react'
 import { Link } from 'react-router-dom';
 
 import Logo from '../../assets/gamaacademy-logo.svg';
-import Error from '../../components/Error';
 
 import { Container, Content, Form } from './style';
 
+import api from '../../services/api';
+
 const SignIn: React.FC = () => {
+  const [,] = useState('');
+  const [,] = useState('');
+
+  function handleLogin(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    
+    api.post(``, {})
+      .then(response => {
+
+      });
+  }
+
   return (
     <Container>
       <img src={Logo} alt="GamaAcademy Logo"/>
       <Content>
-        <Form>
+        <Form onSubmit={handleLogin}>
           <p>Faço seu login</p>
           <input type="text" placeholder="Digite seu usuário" />
           <input type="text" placeholder="Digite sua senha" />
-          <Link to="/dashboard">Continuar</Link>
+          <button type="submit">Continuar</button>
         </Form>
         <Link to="/forgot-password">Esqueci minha senha</Link>
         <Link to="/">Ainda não sou cliente</Link>
       </Content>
-      {/* <Error /> */}
     </Container>
   )
 }
